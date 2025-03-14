845bba21c315b898cb4934a2216905e5ede9a138
Jbarb25 <barboza205@usf.edu>
2025-03-14 08:14:02 -0400

    Task 1 - Base Structure Setup
    This is the html that we will be working with in order to log risk assessments for the business. It is linked to the javascript. The javascript is pulling the container that was established in the html and will add to it as more risk assessments are entered.

---
+++ cc_15.js
@@ -0,0 +1,5 @@
+//Task 1: Select the riskDashboard container and print to the console
+
+const riskDashboardByID = document.getElementById("riskDashboard"); //pull the container by its ID
+const riskDashboardByQuery = document.querySelector('#riskDashboard');  //pull the container by the query's first instance
+console.log('Risk Dashboard Loaded') //print to the console
